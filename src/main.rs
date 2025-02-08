322c736237f2b31acabcbfbf70b904bbd72ae4fd
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2025-02-08 12:06:13 +1000

    Add auth, improve recipe handling

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,95 @@ use std::path::Path;
 use std::fs;
 use std::process::exit;
 // use std::env;
-use json::*;
-use json::JsonValue::Array;
+use serde_json::*;
+use serde_json::Value::Array;
+// use json::JsonValue::Array;
+use clap::Parser;
+use std::collections::HashMap;
 
-fn retrieve_recipe() -> JsonValue {
+#[derive(Parser)]
+#[command(name = "recipe_printer")]
+struct Arguments {
+    /// Base link to Tandoor instance
+    instance: String,
+    
+    /// Recipe ID
+    id: u16,
+
+    /// Username to authenticate with
+    #[arg(short, long)]
+    username: Option<String>,
+
+    /// Password to authenticate with
+    #[arg(short, long)]
+    password: Option<String>,
+
+    /// Token to authenticate with
+    #[arg(short, long)]
+    token: Option<String>,
+
+    // /// Printer path
+    // #[arg(long, default_value_t="/dev/usb/lp0")]
+    // printer_path: String,
+}
+
+fn auth(args: &Arguments) -> String {
+    if args.token.is_some() {
+        return args.token.clone().unwrap();
+    }
+
+    if args.username.is_some() && args.password.is_some() {
+        // Retrieve token
+        let mut auth_deets = HashMap::new();
+        auth_deets.insert("username", args.username.clone().unwrap());
+        auth_deets.insert("password", args.password.clone().unwrap());
+
+        println!("Signing in with {:?} / {:?}", auth_deets.get("username"), auth_deets.get("password"));
+
+        println!("Requesting {}/api-token/auth/", args.instance);
+
+        let auth_client = reqwest::blocking::Client::new();
+
+        let resp = auth_client.post(format!("{}/api-token-auth/", args.instance))
+            .json(&auth_deets)
+            .send();
+        
+        match resp {
+            Ok(r) => {
+                // println!("{}", r.json().expect("No body")["token"]);
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+                exit(2);
+            }
+        }
+        
+    }
+
+    if args.username.is_none() || args.password.is_none() {
+        println!("Please provide username and password!");
+        exit(1);
+    }
+
+    println!("Please provide at least one authentication method!");
+    exit(1);
+}
+
+fn retrieve_recipe(tok: String, id: u16) -> Value {
     let file_path = Path::new("./test.json");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     // println!("{contents}");
-    json::parse(&contents).unwrap()
+    serde_json::from_str(&contents).expect("Malformed recipe")
 }
 
 fn main() -> Result<()> {
-    let recipe = retrieve_recipe();
+    let args = Arguments::parse();
+    // let tok = auth(&args);
+
+    // exit(0);
+
+    let recipe = retrieve_recipe("abcd".to_string(), args.id);
 
     println!("Recipe: {}", recipe["name"]);
 
@@ -27,14 +103,14 @@ fn main() -> Result<()> {
     printer.init().unwrap();
     printer.bold(true)?
         .size(2, 2)?
-        .writeln(&format!("{}", recipe["name"]))?
+        .writeln(&format!("{}", recipe["name"].as_str().unwrap()))?
         .reset_size()?
         .bold(false)?;
 
     let steps;
     match &recipe["steps"] {
         Array(val) => {steps = val},
-        _ => {exit(1)}
+        _ => {exit(2)}
     }
 
     let mut step_no = 0;
@@ -50,21 +126,44 @@ fn main() -> Result<()> {
             let ingredients;
             match &step["ingredients"] {
                 Array(val) => {ingredients = val},
-                _ => {exit(1)}
+                _ => {exit(2)}
             }
             for ingredient in ingredients {
-                println!("- {}{} {}", 
-                    ingredient["conversions"][0]["amount"], 
-                    ingredient["conversions"][0]["unit"], 
-                    ingredient["conversions"][0]["food"]);
-                printer.writeln(&format!("- {}{} {}", 
-                    ingredient["conversions"][0]["amount"], 
-                    ingredient["conversions"][0]["unit"], 
-                    ingredient["conversions"][0]["food"]))?;
+                let mut ingredient_str = String::from("- ");
+                
+                if ingredient["amount"].as_number().is_some() {
+                    let amnt = ingredient["amount"].as_number().unwrap();
+                    if amnt.is_f64() && amnt.as_f64().unwrap() > 0.0 {
+                        ingredient_str.push_str(&format!("{} ", &amnt));
+                    }
+                }
+
+                if ingredient["unit"]["name"].as_str().is_some() {
+                    let unit = ingredient["unit"]["name"].as_str().unwrap();
+                    ingredient_str.push_str(&format!("{} ", &unit));
+                }
+
+                if ingredient["food"]["name"].as_str().is_some() {
+                    let food = ingredient["food"]["name"].as_str().unwrap();
+                    ingredient_str.push_str(&format!("{} ", &food));
+
+                    println!("{}", ingredient_str);
+                    printer.writeln(&ingredient_str)?;
+                }
+                // if ingredient["conversions"].as_array().unwrap().len() > 0 {
+                //     println!("- {}{} {}", 
+                //         ingredient["conversions"][0]["amount"].as_number().unwrap(), 
+                //         ingredient["conversions"][0]["unit"].as_str().unwrap(), 
+                //         ingredient["conversions"][0]["food"].as_str().unwrap());
+                //     printer.writeln(&format!("- {}{} {}", 
+                //         ingredient["conversions"][0]["amount"].as_number().unwrap(), 
+                //         ingredient["conversions"][0]["unit"].as_str().unwrap(), 
+                //         ingredient["conversions"][0]["food"].as_str().unwrap()))?;
+                // }
             }
         }
-        println!("{}", step["instruction"]);
-        printer.writeln(&format!("{}", step["instruction"]))?;
+        println!("{}", step["instruction"].as_str().unwrap());
+        printer.writeln(&format!("{}", step["instruction"].as_str().unwrap()))?;
         printer.feed()?;
     }
 
